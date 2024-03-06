c64b8f98e9b36a2328ee4114c1a9cc7c0b198642
Stephen Walsh <sfwalshappdev@gmail.com>
2024-03-06 00:37:55 +0000

    fixes issue with readingStatus not being updated

--- src/components/ReadingListItemForm.tsx
+++ src/components/ReadingListItemForm.tsx
@@ -85,6 +85,7 @@ const ReadingListItemForm = (props: ReadingListItemFormProps) => {
                     Cancel
                 </button>
             </p>
+            
         </form>
     )
 };
--- src/routes/EditItemView.tsx
+++ src/routes/EditItemView.tsx
@@ -9,17 +9,18 @@ import ReadingListItemForm from "../components/ReadingListItemForm"
 
 const EditItemView = () => {
     const location = useLocation();
-    const { itemToEdit } = location.state;
+    const { itemToEdit } = location.state as { itemToEdit: ReadingListItem};
 
     const navigate = useNavigate();
     const readingListService = container.get<IRepository<ReadingListItem>>(SERVICE_KEYS.READINGLIST_REPOSITORY);
 
     const onSubmitHandler = async (title: string, author: string, readingStatus: ReadingStatus) => {
-        await readingListService.update({ book: { ...itemToEdit.book, title: title, author: author } })
+        await readingListService.update({ book: { ...itemToEdit.book, title: title, author: author }, status: readingStatus })
         navigate(`/`);
     };
 
     const handleCancel = () => {
+        console.log(itemToEdit.status)
         navigate(-1);
     };
 
@@ -27,7 +28,7 @@ const EditItemView = () => {
         <ReadingListItemForm
             title={itemToEdit.book.title}
             author={itemToEdit.book.author}
-            readingStatus={itemToEdit.readingStatus}
+            readingStatus={itemToEdit.status}
             onSubmit={onSubmitHandler}
             onCancel={() => handleCancel()}
             submitButtonText="Update"
